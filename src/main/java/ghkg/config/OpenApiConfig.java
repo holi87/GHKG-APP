747b7dcc12a7974be2b884bba92a7aa9649bc37c
Grzegorz Holak <holi87@gmail.com>
2025-04-18 22:36:11 +0200

    version on actuator from pom

--- src/main/java/ghkg/config/OpenApiConfig.java
+++ src/main/java/ghkg/config/OpenApiConfig.java
@@ -5,19 +5,23 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
 
+    @Value("${build.version:unknown}")
+    private String appVersion;
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
                         .title("GH & KG API")
-                        .version("1.0")
-                        .description("API for testing purposes"))
+                        .description("API for testers")
+                        .version(appVersion))
                 .addSecurityItem(new SecurityRequirement().addList("basicAuth"))
                 .components(new Components().addSecuritySchemes("basicAuth",
                         new SecurityScheme()
